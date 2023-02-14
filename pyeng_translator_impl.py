92154281cb0acefb5889012d6886a4dd0c1c6dd4
UserVanya <general@startsev-ivan.ru>
2023-02-14 22:56:33 +0300

    now it is a class with functions as an interfaces to the api requests

--- pyeng_translator_impl.py
+++ pyeng_translator_impl.py
@@ -1,43 +1,89 @@
 import requests
 import json
 import tkinter as tk
+
+
 class TranslatorImpl:
-    def __init__(self, api_key, folder_id, langs):
+    '''
+    Class with methods to obtain information from yandex cloud service.
+    Main methods are:
+    get_language_code (word: str, hint_lang_codes: dict) -> str;
+    get_translation (word: str, target_lang_code: str) ->str;
+    get_available_langs () -> list;
+    '''
+    def __init__(self, api_key, folder_id):
         self.api_key = api_key
         self.folder_id = folder_id
-        self.langs = langs
-        self.langs_dict = {langs[0]: langs[1], langs[1]: langs[0]}
-    def get_language_code(self, word: str, use_yandex_cloud = False) -> str:
-        if use_yandex_cloud:
-            return self.get_language_code_yacl(word)
-        else:
-            return self.get_language_code_simple(word)
-    def get_language_code_simple(self, word: str) -> str:
-        if word.isascii():
-            return "en"
-        else:
-            return "ru"
-    def get_language_code_yacl(self, word: str) -> str:
-        body = {
-            "folderId": self.folder_id,
-            "languageCodeHints": self.langs,
-            "text": word
+        self.headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Api-Key {0}".format(self.api_key)
         }
-        response_dict, success, msg = self.get_response_dict("detect", body)
+    def get_available_langs(self):
+        '''
+        Returns list of available languages.
+        Return example:
+        {
+            "languages": [
+                    {
+                    "code": "az",
+                    "name": "azərbaycan"
+                    },
+                    {
+                    "code": "en",
+                    "name": "English"
+                    },
+                    ...
+            ]
+        }
+        '''
+        body = {
+            "folderId": self.folder_id
+        }
+        response_dict, success, msg = self.__get_response_dict("languages", body)
         if success:
             return response_dict['languageCode'], True, ""
         else:
             raise Exception(msg)
-    def get_headers(self):
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": "Api-Key {0}".format(self.api_key)
+    def get_language_code(self, word: str, hint_lang_codes = []) -> str:
+        '''
+        Returns the language code for this word. 
+        There are some words that can be ambiguous to determine. Hence, it is better to add some hints as a list of language codes;
+        Return example:
+        get_language_code("привет", 'ru')
+        'ru'
+        '''
+        body = {
+            "folderId": self.folder_id,
+            "languageCodeHints": hint_lang_codes,
+            "text": word
         }
-        return headers
-    def get_response_dict(self, way: str, body) -> tuple:
-        assert way in ["translate", "detect"]
+        response_dict, success, msg = self.__get_response_dict("detect", body)
+        if success:
+            return response_dict['languageCode'], True, ""
+        else:
+            raise Exception(msg)
+    def get_translation(self, word, target_lang_code = 'en') -> str:
+        '''
+        Returns the translation of the given word into the language with target_lang_code (default: 'en')
+        Return example:
+        get_translation ("Hello", 'en')
+        'привет'
+        '''
+        body = {
+            "targetLanguageCode": target_lang_code,
+            "texts": word,
+            "folderId": self.folder_id
+        }
+        response_dict, success, msg = self.__get_response_dict("translate", body)
+        if success:
+            return response_dict['translations'][0]['text']
+        else:
+            raise Exception(msg)
+    def __get_response_dict(self, request_string: str, body) -> tuple:
+        assert request_string in ["translate", "detect", 'languages']
         try:
-            response = requests.post('https://translate.api.cloud.yandex.net/translate/v2/' + way, json=body, headers=self.get_headers())
+            response = requests.post(
+                'https://translate.api.cloud.yandex.net/translate/v2/' + request_string, json=body, headers=self.headers)
         except requests.exceptions.ConnectionError:
             return {}, False, "No internet connection"
         except requests.exceptions.Timeout:
@@ -50,39 +96,12 @@ class TranslatorImpl:
         if "code" in response_dict.keys():
             return {}, False, "Error code: {0}, message: {1}".format(response_dict['code'], response_dict['message'])
         return response_dict, True, "Success"
-    
-    
-    
-    def translate(self, word) -> tuple:
-        body = {
-            "targetLanguageCode": self.langs_dict[self.get_language_code(word, False)],
-            "texts": word,
-            "folderId": self.folder_id
-        }
-        response_dict, success, msg = self.get_response_dict("translate", body)
-        if success:
-            return response_dict['translations'][0]['text']
-        else:
-            raise Exception(msg)
-
-
-
-# def main():
-#     while True:
-#         if keyboard.is_pressed('Ctrl + Q'):
-#             try:
-#                 text = pyperclip.waitForNewPaste(shortcut_timeout)
-#                 print(text)
-#                 translation = translate_to_rus(text)
-#                 print(translation)
-#                 text = pyperclip.waitForNewPaste(shortcut_timeout)
-#                 tk.showinfo("Yandex Translator", translation)
-#             except pyperclip.PyperclipTimeoutException:
-#                 pass
 
 def main():
     impl = TranslatorImpl("AQVNxjK5Yr282hq2cLekOt7lWu7YpMUw6KM5xiLS", "b1gf5shu9k696m00c35h")
-    print(impl.translate("Hello"))
+    print(impl.get_translation("Hello", 'ru'))
+    print(impl.get_language_code("Hello", ['ru', 'en']))
+    print(impl.get_available_langs())
 
 if __name__ == "__main__":
     main()    
